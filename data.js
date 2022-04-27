196032f9cb19c64889041e7b0e9795f144f36149
hyvenn <56298319+hyvenn@users.noreply.github.com>
2022-04-27 14:55:54 +0200

    Starlette visite virtuelle 2

---
+++ data.js
@@ -0,0 +1,409 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-star-vue-des-quais",
+      "name": "STAR vue des quais",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.574374795155002,
+          "pitch": -0.019826208290309566,
+          "rotation": 1.5707963267948966,
+          "target": "2-star-vue-facade-nord"
+        },
+        {
+          "yaw": 1.6274922214326892,
+          "pitch": 0.0003009470257335778,
+          "rotation": 4.71238898038469,
+          "target": "4-star-vue-coeur-dilot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-star-vue-de-la-rue",
+      "name": "STAR vue de la rue",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7033628719005165,
+          "pitch": -0.01204844651698167,
+          "rotation": 1.5707963267948966,
+          "target": "4-star-vue-coeur-dilot"
+        },
+        {
+          "yaw": 1.055163169528619,
+          "pitch": -0.02286175392305978,
+          "rotation": 4.71238898038469,
+          "target": "2-star-vue-facade-nord"
+        },
+        {
+          "yaw": 0.23491900757087514,
+          "pitch": 0.001531342780422662,
+          "rotation": 0,
+          "target": "5-star-vue-terrasse-vatel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-star-vue-facade-nord",
+      "name": "STAR vue facade nord",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.030448364715365273,
+          "pitch": 0.007751472809349735,
+          "rotation": 0,
+          "target": "3-star-vue-parking-velo"
+        },
+        {
+          "yaw": -1.47469766490455,
+          "pitch": -0.021906163221238728,
+          "rotation": 1.5707963267948966,
+          "target": "1-star-vue-de-la-rue"
+        },
+        {
+          "yaw": 1.4552101693191934,
+          "pitch": 0.0045457157855715025,
+          "rotation": 4.71238898038469,
+          "target": "0-star-vue-des-quais"
+        },
+        {
+          "yaw": -1.243743648558027,
+          "pitch": -0.7695505703744558,
+          "rotation": 2.356194490192345,
+          "target": "7-star-vue-terrasse-r8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-star-vue-parking-velo",
+      "name": "STAR vue parking velo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.029908490421791,
+          "pitch": 0.19728364746253746,
+          "rotation": 0,
+          "target": "4-star-vue-coeur-dilot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-star-vue-coeur-dilot",
+      "name": "STAR vue coeur d'ilot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19727169471893724,
+          "pitch": 0.007349477193262288,
+          "rotation": 0,
+          "target": "3-star-vue-parking-velo"
+        },
+        {
+          "yaw": 1.319026232769616,
+          "pitch": -0.004269699761053403,
+          "rotation": 0,
+          "target": "5-star-vue-terrasse-vatel"
+        },
+        {
+          "yaw": -0.2051866661501478,
+          "pitch": -0.726242461621851,
+          "rotation": 3.141592653589793,
+          "target": "6-star-vue-balcon"
+        },
+        {
+          "yaw": 0.7935454769388031,
+          "pitch": -0.8514279304893968,
+          "rotation": 2.356194490192345,
+          "target": "7-star-vue-terrasse-r8"
+        },
+        {
+          "yaw": -2.257038009891037,
+          "pitch": 0.008817507092830468,
+          "rotation": 1.5707963267948966,
+          "target": "0-star-vue-des-quais"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-star-vue-terrasse-vatel",
+      "name": "STAR vue terrasse Vatel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1623167679047608,
+          "pitch": 0.0015538898640254928,
+          "rotation": 3.141592653589793,
+          "target": "4-star-vue-coeur-dilot"
+        },
+        {
+          "yaw": -3.021262843346358,
+          "pitch": -0.015399533105867391,
+          "rotation": 6.283185307179586,
+          "target": "1-star-vue-de-la-rue"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-star-vue-balcon",
+      "name": "STAR vue balcon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5691232909163837,
+          "pitch": 0.6484589717605331,
+          "rotation": 3.141592653589793,
+          "target": "4-star-vue-coeur-dilot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-star-vue-terrasse-r8",
+      "name": "STAR vue terrasse R+8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.867009586822361,
+          "pitch": 0.002399128063824918,
+          "rotation": 3.141592653589793,
+          "target": "5-star-vue-terrasse-vatel"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
